21b12a18354bc37527a88de2240d84b57fa564f3
pankowskitomasz <pankowski.email@gmail.com>
2025-03-30 12:09:16 +0200

    routing,layout: signin,signup,restorepassword,passwordupdate

---
+++ src/views/category/categoryDetailsView.js
@@ -0,0 +1,12 @@
+import {Container } from "react-bootstrap";
+
+function CategoryDetailsView(){
+    return(
+        <Container>
+            <h1>Category Details View</h1>
+
+        </Container>
+    );
+}
+
+export default CategoryDetailsView;
---
+++ src/views/categoryView.js
@@ -0,0 +1,12 @@
+import {Container } from "react-bootstrap";
+
+function CategoryView(){
+    return(
+        <Container>
+            <h1>Category View</h1>
+
+        </Container>
+    );
+}
+
+export default CategoryView;
